88a71413befbf1d18efb8e7de6c318d4cb4a1b42
wilbrone <wilbroneokoth@gmail.com>
2020-04-01 01:50:54 +0300

    add styling content detail page

--- resources/views/contents/contDetails.blade.php
+++ resources/views/contents/contDetails.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
     <div class="container" style="width:95%;border:solid;margin:0 auto;min-height:80%;">
-        <h2>{{ $data->title }}</h2>
-        {{ $data->content }}
+        <h2 style="font-size:2em; margin-bottom:3rem;"><b>{{ $data->title }}</b></h2>
+        <p style="margin-bottom:2rem;">{{ $data->tag }}</p>
+        <p style="margin-bottom:2rem;">{{ $data->content }}</p>
+        <p style="margin-bottom:2rem;"><em>Posted By</em> <b>{{ $data->user_id }}</b></p>
     </div>
 @endsection
